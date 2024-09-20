37e07e65fc0bb6ae1f3f67c88ccd87224b5ecd5c
Priyank119 <67916572+Priyank119@users.noreply.github.com>
2024-09-20 23:54:06 +0530

    Shader Done
    hi

--- Assets/Scripts/Mremireh.cs
+++ Assets/Scripts/Mremireh.cs
@@ -1,17 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Mremireh : MonoBehaviour
 {
     private ScoreUI _scoreUI;
     public float getScore;
+    public AudioClip _hit;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "arrow")
         {
             _scoreUI.getTotalScore();
+            this.GetComponent<AudioSource>().PlayOneShot(_hit);
+            this.GetComponent<Animator>().SetTrigger("death");
+            float desolve = Mathf.Lerp(-1f, 1f, 0.5f);
+            this.GetComponent<MeshRenderer>().material.SetFloat("_Desolve", desolve);
         }
     }
 }
